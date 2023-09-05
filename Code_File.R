71d7e97b63e25f853e59e40ba36a2eab28786836
s172084 <s172084@dtu.dk>
2023-09-05 17:14:13 +0200

    1 line changed.

--- Code_File.R
+++ Code_File.R
@@ -1,7 +1,7 @@
 library(usethis)
 usethis::use_git_config(user.name = "s172084", user.email = "s172084@dtu.dk")
 usethis::create_github_token()
-# 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
+
 
 # Hello!
 # Hi
