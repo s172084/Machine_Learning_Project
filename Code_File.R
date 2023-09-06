72307b0f30aadfb1302b70fc777fd732bb52fc96
s172084 <s172084@dtu.dk>
2023-09-06 16:38:02 +0200

    A range of different datasets for classification and regression.

---
+++ Code_File.R
@@ -0,0 +1,32 @@
+
+# -- Mass datasets
+library(MASS)
+
+# -- Cabbages
+data(cabbages)
+cabbages
+?cabbages
+
+# -- Crabs
+data(crabs)
+crabs
+?crabs
+
+# -- Diamonds
+library(tidyverse)
+data(diamonds)
+?diamonds
+diamonds
+
+
+#-- Glass 
+library(mlbench)
+data(Glass)
+?Glass
+Glass
+
+#-- Cars
+library(ISLR)
+data(Auto)
+?Auto
+Auto
