f75d42a71a6d7da9ceeead4dd400c0c364c98033
PietroLombardo <10669058@polimi.it>
2023-09-15 16:02:25 +0200

    Canceled other datasets

--- Code_File.R
+++ Code_File.R
@@ -2,25 +2,9 @@
 # -- Mass datasets
 library(MASS)
 
-# -- Crabs
-data(crabs)
-crabs
-?crabs
-
 # -- Diamonds
 library(tidyverse)
 data(diamonds)
 ?diamonds
 diamonds
 
-#-- Glass 
-library(mlbench)
-data(Glass)
-?Glass
-Glass
-
-#-- Cars
-library(ISLR)
-data(Auto)
-?Auto
-Auto
