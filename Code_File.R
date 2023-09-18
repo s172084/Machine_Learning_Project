2e6e20c7054d152e2b4ff09693f53c9aee32d1f6
PietroLombardo <10669058@polimi.it>
2023-09-18 18:21:57 +0200

    Report question 3 started, 4 sections of the Project1.pdf file implemented in the report, PCA at 70% of the progress

--- Code_File.R
+++ Code_File.R
@@ -3,27 +3,74 @@
 library(MASS)
 library(ggplot2)
 library(data.table)
+library(mltools)
 library(reshape)
 library(reshape2)
+library(rmarkdown)
+library(tidyverse)
+
+render("C:/Users/Alessandro/Desktop/PIETRO/Università/3_Machine Learning and Data Mining/Project/Machine_Learning_Project/Report.Rmd")
 
 # -- Diamonds
-library(tidyverse)
 data(diamonds)
 ?diamonds
 str(diamonds)
 
+# One-hot-encode for regression of price ###
+# Maybe it is not necessary to transform in one-hot-encode because all the 3 factor
+# attributes are ordered (we know that cut "fair" is worse than cat "good")
 
+D <- as.data.frame(diamonds[-c(2:4,7)]) # drop "price", that is the target variable, and nominal variables
+
+cut_names <- as.character(levels(diamonds$cut))
+onehot_cut <- as.matrix(one_hot(as.data.table(factor(as.character(diamonds$cut)))))
+colnames(onehot_cut) <- make.names(cut_names)
+D <- as.matrix(cbind(D, onehot_cut))
+
+color_names <- as.character(levels(diamonds$color))
+onehot_color <- as.matrix(one_hot(as.data.table(factor(as.character(diamonds$color)))))
+colnames(onehot_color) <- make.names(color_names)
+D <- as.matrix(cbind(D, onehot_color))
+
+clarity_names <- as.character(levels(diamonds$clarity))
+onehot_clarity <- as.matrix(one_hot(as.data.table(factor(as.character(diamonds$clarity)))))
+colnames(onehot_clarity) <- make.names(clarity_names)
+D <- as.matrix(cbind(D, onehot_clarity))
+
+N <- dim(D)[1] # number of rows of the new one-hot-encoded dataframe
+M <- dim(D)[2] # number of columns of the new one-hot-encoded dataframe
+
+head(D)
 
 # PCA ##########################################################################
 
-D_pca <- diamonds[,-c(2:4,7)]
-colMeans(D_pca)
-D_pca <- t(apply(D_pca, 1,"-",colMeans(D_pca)))
+Dia <- as.data.frame(diamonds[-7]) # drop the target variable "price"
+Dia$cut <- as.numeric(Dia$cut)
+Dia$color <- as.numeric(Dia$color)
+Dia$clarity <- as.numeric(Dia$clarity)
+
+N <- dim(Dia)[1] # number of rows of the new one-hot-encoded dataframe
+M <- dim(Dia)[2] # number of columns of the new one-hot-encoded dataframe
+
+head(Dia)
+
+stds <- apply(Dia, 2, sd)
+par(mfrow = c(1, 1))
+barplot(stds, ylab = "Diamonds: attribute standard deviation")
+
+# Stardardization of the dataset:
+# For each column: subtract the mean (of the col) and divide by the std (of the col)
+D_pca <- t(apply(Dia, 1, "-", colMeans(Dia)))
+D_pca <- t(apply(D_pca, 1, "*", 1 / stds))
+
+head(D_pca)
+
+# Singular value decomposition
 S <- svd(D_pca)
 diagS <- S$d
 rho <- diagS^2 / sum(diagS^2)
 
-threshold <- 0.95
+threshold <- 0.9
 
 xlimits <- c(1, M)
 plot(rho,
@@ -45,8 +92,17 @@ legend("right", # Define position
        cex = 1, bg = "lightblue"
 ) # Setup how the box looks (cex controls size)
 
-# Manual projecting data onto principal component.
+# Principal directions interpreted in terms of features
 Z <- S$u %*% diag(S$d)
+V <- S$v
+
+pcs <- 1:2
+test <- as.data.frame(melt(data.table(V[, pcs])))
+ggplot(test, aes(x = rep(1:9, length(pcs)), y = value, fill=variable)) +
+  geom_bar(position="dodge", stat = "identity") +
+  labs(fill="PC", x = "Attributes", y = "Component coefficients")
+
+# Data projected onto the first two principal components
 
 i <- 1
 j <- 2
@@ -55,12 +111,3 @@ ggplot() +
   geom_point(aes(x = Z[, i], y = Z[, j]), size = 1, alpha = 0.5) +
   theme(legend.position = c(0.88, 0.22), legend.title = element_blank()) +
   labs(x = colnames(Z)[i], y = colnames(Z)[j])
-
-
-V <- S$v
-
-pcs <- 1:2
-test <- as.data.frame(melt(data.table(V[, pcs])))
-ggplot(test, aes(x = rep(1:6, length(pcs)), y = value, fill=variable)) +
-  geom_bar(position="dodge", stat = "identity") +
-  labs(fill="PC", x = "Attributes", y = "Component coefficients")
